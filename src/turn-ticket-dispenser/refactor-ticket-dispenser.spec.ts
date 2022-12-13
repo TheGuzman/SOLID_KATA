d02be054eb0f92c43771beca7204c84ea9bba289
TheGuzman <guzmanmedrano.diego@gmail.com>
2022-12-13 13:27:13 +0100

    feat(tests): added refactored tests

--- src/turn-ticket-dispenser/refactor-ticket-dispenser.spec.ts
+++ src/turn-ticket-dispenser/refactor-ticket-dispenser.spec.ts
@@ -2,16 +2,17 @@ import TicketDispenser from "../turn-ticket-dispenser/ticket-dispenser";
 import TurnNumberSequence from "../turn-ticket-dispenser/turn-number-sequence";
 
 describe("Given a single ticket Dispenser", () => {
+    let startSequenceNumber = 0;
+    let turnSequence: TurnNumberSequence;
+    let dispenser: TicketDispenser;
+    beforeEach(() => {
+        turnSequence = new TurnNumberSequence(startSequenceNumber);
+        dispenser = new TicketDispenser(turnSequence);
+    });
     describe("When a user gets a ticket", () => {
-        let turnSequence: TurnNumberSequence;
-        let dispenser: TicketDispenser;
-        beforeEach(() => {
-            turnSequence = new TurnNumberSequence();
-            dispenser = new TicketDispenser(turnSequence);
-        });
         it("it should be one number greater or equal than cero", () => {
             const ticket = dispenser.getTurnTicket();
-            expect(ticket.ticketNumber).toBe(0);
+            expect(ticket.ticketNumber).toBe(startSequenceNumber);
         });
         it("and then another user gets a ticket, it should be one number greater than the previous ticket", () => {
             const prevTicket = dispenser.getTurnTicket();
@@ -22,19 +23,20 @@ describe("Given a single ticket Dispenser", () => {
         });
     });
 });
-describe("Given a multiple ticket Dispensers", () => {
+describe("Given multiple ticket Dispensers", () => {
+    let startSequenceNumber = 0;
+    let turnSequence: TurnNumberSequence;
+    let firstDispenser: TicketDispenser;
+    let secondDispenser: TicketDispenser;
+    beforeEach(() => {
+        turnSequence = new TurnNumberSequence(startSequenceNumber);
+        firstDispenser = new TicketDispenser(turnSequence);
+        secondDispenser = new TicketDispenser(turnSequence);
+    });
     describe("When a user gets a ticket", () => {
-        let turnSequence: TurnNumberSequence;
-        let firstDispenser: TicketDispenser;
-        let secondDispenser: TicketDispenser;
-        beforeEach(() => {
-            turnSequence = new TurnNumberSequence();
-            firstDispenser = new TicketDispenser(turnSequence);
-            secondDispenser = new TicketDispenser(turnSequence);
-        });
         it("it should be one number greater or equal than cero", () => {
             const ticket = firstDispenser.getTurnTicket();
-            expect(ticket.ticketNumber).toBe(0);
+            expect(ticket.ticketNumber).toBe(startSequenceNumber);
         });
         it("and then another user gets a ticket from another dispenser, it should be one number greater than the previous ticket", () => {
             const prevTicket = firstDispenser.getTurnTicket();
